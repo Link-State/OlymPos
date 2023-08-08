de44aac32554e984565103c3f71ca43ec9edb6ed
링크상태 <60739875+Link-State@users.noreply.github.com>
2023-08-08 17:50:01 +0900

    fix: 로그인 상태 수정
    '로그인 상태'를 관리자 계정 기준이 아닌 매장 기준으로 하도록 DB를 수정

--- backend/models/init.py
+++ backend/models/init.py
@@ -17,8 +17,8 @@ command.execute("DROP TABLE IF EXISTS Store_info;")
 command.execute("DROP TABLE IF EXISTS Admins;")
 
 # 테이블 생성
-command.execute("CREATE TABLE Admins (unique_admin INT, user_id VARCHAR(32) NOT NULL, user_pwd VARCHAR(32) NOT NULL, name VARCHAR(128) NOT NULL, phone_number VARCHAR(11) NOT NULL, email VARCHAR(128) NOT NULL, isLogin INT);")
-command.execute("CREATE TABLE Store_info (unique_store_info INT, unique_admin INT NOT NULL, store_name VARCHAR(64) NOT NULL, store_owner VARCHAR(128) NOT NULL, store_address VARCHAR(64) NOT NULL, store_tel_number VARCHAR(11) NOT NULL, table_count INT NOT NULL);")
+command.execute("CREATE TABLE Admins (unique_admin INT, user_id VARCHAR(32) NOT NULL, user_pwd VARCHAR(32) NOT NULL, name VARCHAR(128) NOT NULL, phone_number VARCHAR(11) NOT NULL, email VARCHAR(128) NOT NULL);")
+command.execute("CREATE TABLE Store_info (unique_store_info INT, unique_admin INT NOT NULL, store_name VARCHAR(64) NOT NULL, store_owner VARCHAR(128) NOT NULL, store_address VARCHAR(64) NOT NULL, store_tel_number VARCHAR(11) NOT NULL, table_count INT NOT NULL, isLogin INT);")
 command.execute("CREATE TABLE Table_list (unique_store_info INT, table_number INT, table_state INT NOT NULL, isLogin INT);")
 command.execute("CREATE TABLE Product_group (unique_product_group INT, unique_store_info INT NOT NULL, group_name VARCHAR(64) NOT NULL);")
 command.execute("CREATE TABLE Product (unique_product INT, unique_store_info INT NOT NULL, unique_product_group INT NOT NULL, product_name VARCHAR(64) NOT NULL, price INT NOT NULL, image LONGTEXT, description LONGTEXT, amount INT NOT NULL);")
