68177f614f43177b162a200ea4ad400700729f8d
LinkState <jjyyhh99@naver.com>
2023-12-31 13:56:58 +0900

    Form: Store_info 테이블 ORM 방식으로 수정

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ from routes import order
 from routes import version
 from models.mysql import DB
 from models.Admins import Admins
+from models.StoreInfo import StoreInfo
 
 # flask
 # flask_restful
@@ -73,11 +74,35 @@ def test() :
         print(i.disable_date)
         print()
 
+    store = StoreInfo(admin=admin.unique_admin, name="우하하 가게", owner="칡칡이", address="음메민국 칡소시 발굽로 123-2", number="033-123-3333", count=4)
+    DB.session.add(store)
+    DB.session.commit()
+
+    bbbb = StoreInfo.query.all()
+
+    for i in bbbb :
+        print(i.unique_store_info)
+        print(i.unique_admin)
+        print(i.store_name)
+        print(i.store_owner)
+        print(i.store_address)
+        print(i.store_tel_number)
+        print(i.table_count)
+        print(i.last_modify_date)
+        print(i.disable_date)
+        print()
+
     # 삭제
     admin = Admins.query.filter_by(name="dummy")
 
     if admin.count() == 1 :
-        DB.session.delete(admin)
+        DB.session.delete(admin.first())
+        DB.session.commit()
+
+    store = StoreInfo.query.filter_by(store_name="우하하 가게")
+
+    if store.count() == 1 :
+        DB.session.delete(store.first())
         DB.session.commit()
 
     print("\n\n\n\n\n")
--- backend/models/StoreInfo.py
+++ backend/models/StoreInfo.py
@@ -4,9 +4,35 @@ import datetime
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
+from config import *
 from models import mysql
-from models import TableList
+from models.mysql import DB
+from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql.schema import Column
 
+class StoreInfo(DB.Model) :
+    __tablename__ = "Store_info"
+
+    unique_store_info = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    unique_admin = Column(Integer, ForeignKey('Admins.unique_admin'))
+    store_name = Column(String(MaxLength.store_name), nullable=False)
+    store_owner = Column(String(MaxLength.store_owner), nullable=False)
+    store_address = Column(String(MaxLength.store_address), nullable=False)
+    store_tel_number = Column(String(MaxLength.store_tel_number), nullable=False)
+    table_count = Column(Integer, nullable=False)
+    last_modify_date = Column(DateTime, nullable=True, default=datetime.datetime.now())
+    disable_date = Column(DateTime, nullable=True, default=None)
+
+    def __init__(self, admin, name, owner, address, number, count, last=None, disable=None) :
+        self.unique_admin = admin
+        self.store_name = name
+        self.store_owner = owner
+        self.store_address = address
+        self.store_tel_number = number
+        self.table_count = count
+        self.last_modify_date = last
+        self.disable_date = disable
+    
 def findStore(name="") :
     sql = f"""
     SELECT unique_store_info
