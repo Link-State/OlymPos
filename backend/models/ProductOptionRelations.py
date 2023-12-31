2bb9a1eea704b042d0e8bef0d53ad579eb03b53a
LinkState <jjyyhh99@naver.com>
2023-12-31 15:57:03 +0900

    Form: ProductOptionRelations 테이블 ORM 방식으로 수정

--- backend/models/ProductOptionRelations.py
+++ backend/models/ProductOptionRelations.py
@@ -3,7 +3,21 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
+from config import *
 from models import mysql
+from models.mysql import DB
+from sqlalchemy import Column, Integer, ForeignKey
+
+class ProductOptionRelations(DB.Model) :
+    __tablename__ = "Product_Option_relations"
+
+    unique_product = Column(Integer, ForeignKey('Product.unique_product'), primary_key=True, nullable=False)
+    unique_product_option = Column(Integer, ForeignKey('Product_option'), primary_key=True, nullable=False)
+
+    def __init__(self, product, option) :
+        self.unique_product = product
+        self.unique_product_option = option
+
 
 def getOption(product_uid=-1, option_uid=-1) :
     return
--- backend/models/TableList.py
+++ backend/models/TableList.py
@@ -12,12 +12,13 @@ class TableList(DB.Model) :
     __tablename__ = "Table_list"
 
     unique_store_info = Column(Integer, ForeignKey('Store_info.unique_store_info'), primary_key=True, nullable=False)
-    table_number = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    table_number = Column(Integer, primary_key=True, nullable=False)
     table_state = Column(Integer, nullable=False)
     isLogin = Column(Text, nullable=False, default="")
     disable_date = Column(DateTime, nullable=True, default=None)
 
-    def __init__(self, number, state, login="", disable=None) :
+    def __init__(self, store, number, state, login="", disable=None) :
+        self.unique_store_info = store
         self.table_number = number
         self.table_state = state
         self.isLogin = login
