bd29ac402ba7399d68ca26919c11377eff01b523
LinkState <jjyyhh99@naver.com>
2023-12-31 16:13:00 +0900

    Form: OrderList 테이블 ORM 방식으로 수정

--- backend/models/OrderList.py
+++ backend/models/OrderList.py
@@ -6,6 +6,27 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from config import *
 from models import mysql
+from models.mysql import DB
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
+
+class OrderList(DB.Model) :
+    __tablename__ = "Order_list"
+
+    unique_order = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    unique_store_info = Column(Integer, ForeignKey('Store_info.unique_store_info'), nullable=False)
+    unique_product = Column(Integer, ForeignKey('Product.unique_product'), nullable=False)
+    table_number = Column(Integer, ForeignKey('Table_list.table_number'), nullable=True)
+    amount = Column(Integer, nullable=False)
+    order_state = Column(Integer, nullable=False)
+    order_date = Column(DateTime, nullable=False)
+
+    def __init__(self, store, product, number, amount, state, date) :
+        self.unique_store_info = store
+        self.unique_product = product
+        self.table_number = number
+        self.amount = amount
+        self.order_state = state
+        self.order_date = date
 
 def findOrder(store_uid=-1, date=datetime.datetime.now()) :
 
