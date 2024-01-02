86a49fb7edd2940ef247fd9c9c3f7a549cbe7b81
링크상태 <60739875+Link-State@users.noreply.github.com>
2024-01-02 17:32:24 +0900

    Refactor: delete-store, ORM 방식으로 코드 수정

--- backend/controllers/store.py
+++ backend/controllers/store.py
@@ -21,6 +21,7 @@ from models.ProductSuboption import ProductSuboption
 from models.OrderList import OrderList
 from models.SelectedOption import SelectedOption
 from models.Version import Version
+from helpers.store import *
 
 def checkField(data) :
     keyword = []
@@ -46,7 +47,7 @@ def checkField(data) :
             keyword.append("tel_num")
 
     if "count" in data :
-        if data["count"] < 1 :
+        if data["count"] < 0 :
             keyword.append("count")
 
     return keyword
@@ -147,54 +148,56 @@ def change_store_info(inputStoreInfo={}) :
         return {"result" : "Invalid", "code" : Code.NotEquals}
     
     # 매장 정보 수정
-    if "name" in inputStoreInfo :
-        store.store_name = inputStoreInfo["name"]
+    modify_store(store=store, inputStoreInfo=inputStoreInfo)
 
-    if "owner" in inputStoreInfo :
-        store.store_owner = inputStoreInfo["owner"]
+    # if "name" in inputStoreInfo :
+    #     store.store_name = inputStoreInfo["name"]
 
-    if "address" in inputStoreInfo :
-        store.store_address = inputStoreInfo["address"]
+    # if "owner" in inputStoreInfo :
+    #     store.store_owner = inputStoreInfo["owner"]
 
-    if "tel_num" in inputStoreInfo :
-        store.store_tel_number = inputStoreInfo["tel_num"]
+    # if "address" in inputStoreInfo :
+    #     store.store_address = inputStoreInfo["address"]
+
+    # if "tel_num" in inputStoreInfo :
+    #     store.store_tel_number = inputStoreInfo["tel_num"]
     
-    if "count" in inputStoreInfo :
-        tables = TableList.query.filter_by(unique_store_info=store.unique_store_info).order_by(TableList.table_number).all()
+    # if "count" in inputStoreInfo :
+    #     tables = TableList.query.filter_by(unique_store_info=store.unique_store_info).order_by(TableList.table_number).all()
 
-        current = store.table_count # 현재 활성화된 테이블 갯수
-        maximum = len(tables) # DB에 생성된 테이블 갯수
-        request = inputStoreInfo["count"] # 활성화할 테이블 갯수
-        now = datetime.now()
-        now_lnt = int(now.strftime('%Y%m%d%H%M%S%f')[:-3]) # 현재 시간
+    #     current = store.table_count # 현재 활성화된 테이블 갯수
+    #     maximum = len(tables) # DB에 생성된 테이블 갯수
+    #     request = inputStoreInfo["count"] # 활성화할 테이블 갯수
+    #     now = datetime.now()
+    #     now_lnt = int(now.strftime('%Y%m%d%H%M%S%f')[:-3]) # 현재 시간
 
-        # 현재 활성화된 테이블 갯수와 활성화할 테이블 갯수가 같지 않을 때, DB상 기록된 테이블 갯수 수정
-        if request != current :
-            store.table_count = request
-            store.last_modify_date = now
+    #     # 현재 활성화된 테이블 갯수와 활성화할 테이블 갯수가 같지 않을 때, DB상 기록된 테이블 갯수 수정
+    #     if request != current :
+    #         store.table_count = request
+    #         store.last_modify_date = now
 
-            version = Version.query.get(store.unique_store_info)
-            version.table_list = now_lnt
+    #         version = Version.query.get(store.unique_store_info)
+    #         version.table_list = now_lnt
         
-        # 활성화할 테이블 갯수가 현재 활성화된 테이블 갯수보다 클 경우,
-        if request > current :
-            for i in range(current + 1, maximum + 1) :
-                # 재활성화
-                tables[i-1].disable_date = None
-            for i in range(maximum + 1, request + 1) :
-                # 생성
-                table = TableList(store=store.unique_store_info, number=i, state=0)
-                DB.session.add(table)
+    #     # 활성화할 테이블 갯수가 현재 활성화된 테이블 갯수보다 클 경우,
+    #     if request > current :
+    #         for i in range(current + 1, maximum + 1) :
+    #             # 재활성화
+    #             tables[i-1].disable_date = None
+    #         for i in range(maximum + 1, request + 1) :
+    #             # 생성
+    #             table = TableList(store=store.unique_store_info, number=i, state=0)
+    #             DB.session.add(table)
 
-        # 활성화할 테이블 갯수가 현재 활성화된 테이블 갯수보다 작을 경우,
-        elif request < current :
-            # 비활성화
-            for i in range(request + 1, current + 1) :
-                tables[i-1].isLogin = ""
-                tables[i-1].table_state = 0
-                tables[i-1].disable_date = now
+    #     # 활성화할 테이블 갯수가 현재 활성화된 테이블 갯수보다 작을 경우,
+    #     elif request < current :
+    #         # 비활성화
+    #         for i in range(request + 1, current + 1) :
+    #             tables[i-1].isLogin = ""
+    #             tables[i-1].table_state = 0
+    #             tables[i-1].disable_date = now
     
-    DB.session.commit()
+    # DB.session.commit()
 
     return {"result" : "Success", "code" : Code.Success}
     
@@ -203,29 +206,38 @@ def delete_store(inputStoreInfo={}) :
     if "store_uid" not in inputStoreInfo :
         return {"result" : "Invalid", "code" : Code.MissingRequireField}
     
-    store = StoreInfo.getStore(uid=inputStoreInfo["store_uid"])
+    store = StoreInfo.query.get(inputStoreInfo["store_uid"])
 
     # 매장이 존재하지 않는 경우,
-    if len(store) <= 0 :
+    if store == None :
         return {"result" : "Invalid", "code" : Code.NotExistStore}
     
-    uid = Admins.findUID(id=inputStoreInfo["user_id"])
+    user = Admins.query.filter_by(user_id=inputStoreInfo["user_id"]).first()
 
     # 해당 유저가 존재하지 않을 경우,
-    if uid == -1 :
+    if user == None :
         return {"result" : "Invalid", "code" : Code.NotExistID}
     
     # 요청한 사람의 매장이 아닌 경우,
-    if uid != store["unique_admin"] :
+    if user.unique_admin != store.unique_admin :
         return {"result" : "Invalid", "code" : Code.NotEquals}
     
+    now = datetime.now()
+
     # 매장 삭제
-    StoreInfo.remove(uid=inputStoreInfo["store_uid"])
+    store.disable_date = now
 
     # 테이블 삭제
-    for i in range(1, store["table_count"]+1) :
-        TableList.remove(store_uid=inputStoreInfo["store_uid"], tableNum=i)
+    modify_store(store=store, inputStoreInfo={"count" : 0})
 
+    # tables = TableList.query.filter_by(unique_store_info=inputStoreInfo["store_uid"]).order_by(TableList.table_number).all()
+    # for i in range(1, store.table_count + 1) :
+    #     TableList.remove(store_uid=inputStoreInfo["store_uid"], tableNum=i)
+    #     tables[i-1].disable_date = now
+        
+
+    # 해당 모듈 추가 후, 알맞는 삭제함수 호출할 것
+        
     # 상품 그룹 삭제
     ## 매장 고유번호로 그룹 찾을 것.
 
