b98cce01b10c5396941f5cba1495bba72e58c59d
15balloon <jsh159197@gmail.com>
2024-03-22 23:50:46 +0900

    Feat. 상품 관련 API 인터페이스 추가

--- app/app/src/main/java/com/kmyth/olympos/network/ServerCallInterface.kt
+++ app/app/src/main/java/com/kmyth/olympos/network/ServerCallInterface.kt
@@ -4,10 +4,15 @@ import com.kmyth.olympos.model.login.UserLoginRequestModel
 import com.kmyth.olympos.model.login.UserLoginResponseModel
 import com.kmyth.olympos.model.login.TableLoginRequestModel
 import com.kmyth.olympos.model.login.TableLoginResponseModel
+import com.kmyth.olympos.model.product.GroupListResponseModel
+import com.kmyth.olympos.model.product.OptionListResponseModel
+import com.kmyth.olympos.model.product.ProductListResponseModel
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.QueryMap
 
 interface ServerCallInterface {
 
@@ -24,5 +29,26 @@ interface ServerCallInterface {
     @Headers("Content-Type: application/json")
     @POST("table-login")
     fun tableLogin(@Body tableInfo: TableLoginRequestModel): Call<TableLoginResponseModel>
+
+    /**
+     * 상품 카테고리 목록 불러옴
+     */
+    @Headers("Content-Type: application/json")
+    @GET("get-group-list")
+    fun getGroupList(@QueryMap storeInfo: Map<String, Any>): Call<GroupListResponseModel>
+
+    /**
+     * 상품 목록 불러옴
+     */
+    @Headers("Content-Type: application/json")
+    @GET("get-product-list")
+    fun getProductList(@QueryMap storeInfo: Map<String, Any>): Call<ProductListResponseModel>
+
+    /**
+     * 옵션 목록 불러옴
+     */
+    @Headers("Content-Type: application/json")
+    @GET("get-option-list")
+    fun getOptionList(@QueryMap storeInfo: Map<String, Any>): Call<OptionListResponseModel>
 
 }
