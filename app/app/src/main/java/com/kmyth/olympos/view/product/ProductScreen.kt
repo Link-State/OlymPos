bd499f18fccb6c0ea87ae667c9f4566dba4a0b28
15balloon <jsh159197@gmail.com>
2024-03-29 00:43:16 +0900

    Feat: 상품 상세 정보 화면 개발

--- app/app/src/main/java/com/kmyth/olympos/view/product/ProductScreen.kt
+++ app/app/src/main/java/com/kmyth/olympos/view/product/ProductScreen.kt
@@ -1,17 +1,21 @@
 package com.kmyth.olympos.view.product
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -21,6 +25,7 @@ import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,7 +34,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
@@ -39,6 +46,8 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.graphics.createBitmap
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
@@ -74,6 +83,14 @@ fun ProductScreen(
             it.unique_product_group == groupIdx
         }
     ) }
+    var product by remember { mutableStateOf(
+        // TODO : tmp Data
+        ProductModel(0, 0, 0,
+            "tmp", 10000, "", "desc",
+            0, emptyList()
+        )
+    ) }
+    var showProductDetail by remember { mutableStateOf(false) }
     val onGroupChanged: (Int) -> Unit = { index ->
         groupIdx = index
 
@@ -84,6 +101,13 @@ fun ProductScreen(
         } else {
             productList = emptyList()
         }
+    }
+    val onProductClick: (ProductModel) -> Unit = { model ->
+        product = model
+        showProductDetail = true
+    }
+    val onDialogDismiss: () -> Unit = {
+        showProductDetail = false
     }
 
     LaunchedEffect(Unit) {
@@ -98,8 +122,13 @@ fun ProductScreen(
         ProductView(
             groupListState.value,
             onGroupChanged,
-            productList
+            productList,
+            onProductClick
         )
+    }
+
+    if (showProductDetail) {
+        ProductDetailDialog(product, onDialogDismiss)
     }
 }
 
@@ -107,7 +136,8 @@ fun ProductScreen(
 fun ProductView(
     groupList: List<GroupModel>, 
     onGroupChanged: (Int) -> Unit, 
-    productList: List<ProductModel>
+    productList: List<ProductModel>,
+    onProductClick: (ProductModel) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -115,7 +145,7 @@ fun ProductView(
             .fillMaxHeight()
     ) {
         GroupListRow(groupList, onGroupChanged)
-        ProductListBox(productList)
+        ProductListBox(productList, onProductClick)
     }
 }
 
@@ -124,7 +154,7 @@ fun GroupListRow(
     groupList: List<GroupModel>, 
     onGroupChanged: (Int) -> Unit
 ) {
-    LazyRow() {
+    LazyRow {
         itemsIndexed(groupList) { _, group ->
             Button(
                 modifier = Modifier
@@ -143,7 +173,10 @@ fun GroupListRow(
 }
 
 @Composable
-fun ProductListBox(productList: List<ProductModel>) {
+fun ProductListBox(
+    productList: List<ProductModel>,
+    onProductClick: (ProductModel) -> Unit
+) {
     LazyVerticalGrid(
         modifier = Modifier
             .fillMaxSize()
@@ -160,7 +193,8 @@ fun ProductListBox(productList: List<ProductModel>) {
                     .height(224.dp),
                 contentPadding = PaddingValues(16.dp),
                 shape = RoundedCornerShape(8.dp),
-                onClick = {}) {
+                onClick = { onProductClick(product) }
+            ) {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -174,27 +208,179 @@ fun ProductListBox(productList: List<ProductModel>) {
                         text = product.product_name,
                         fontSize = 24.sp
                     )
-                    Box(
-                        modifier = Modifier
-                            .width(192.dp)
-                            .height(108.dp)
-                            .align(Alignment.Center)
-                            .background(Color.Gray)
-                    )
-                    Image(
-                        modifier = Modifier
-                            .width(192.dp)
-                            .height(108.dp)
-                            .align(Alignment.Center),
-                        bitmap = decodedByte.asImageBitmap(),
-                        contentDescription = "Image of ${product.product_name}",
-                        contentScale = ContentScale.Fit
+                    ProductImage(
+                        width = 192,
+                        height = 108,
+                        imageBitmap = decodedByte,
+                        name = product.product_name
                     )
                     Text(
                         modifier = Modifier.align(Alignment.BottomEnd),
                         text = "${product.price}",
                         fontSize = 24.sp
                     )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductImage(
+    width: Int,
+    height: Int,
+    imageBitmap: Bitmap,
+    name: String
+) {
+    Box {
+        Box(
+            modifier = Modifier
+                .width(width.dp)
+                .height(height.dp)
+                .align(Alignment.Center)
+                .background(Color.Gray)
+        )
+        Image(
+            modifier = Modifier
+                .width(width.dp)
+                .height(height.dp)
+                .align(Alignment.Center),
+            bitmap = imageBitmap.asImageBitmap(),
+            contentDescription = "Image of $name",
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ProductDetailDialog(
+    product: ProductModel,
+    onDialogDismiss: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDialogDismiss,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false
+        ),
+        content = {
+            ProductDetailDialogContent(
+                product,
+                onDialogDismiss
+            )
+        }
+    )
+}
+
+@Composable
+fun ProductDetailDialogContent(
+    product: ProductModel,
+    onDialogDismiss: () -> Unit
+) {
+    val decodedString = Base64.decode(product.image, Base64.DEFAULT) ?: ByteArray(0)
+    val decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size) ?: createBitmap(224, 224)
+
+    Surface(
+        modifier = Modifier
+            .width(1120.dp)
+            .height(576.dp),
+        color = Color.White
+    ) {
+        Row(
+            modifier = Modifier.padding(44.dp)
+        ) {
+            Column(
+                modifier = Modifier.width(548.dp)
+            ) {
+                Text(
+                    text = product.product_name,
+                    fontSize = 40.sp
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                ProductImage(
+                    width = 548,
+                    height = 308,
+                    imageBitmap = decodedByte,
+                    name = product.product_name
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = product.description,
+                    fontSize = 24.sp
+                )
+            }
+            Spacer(modifier = Modifier.width(76.dp))
+            Column(
+                modifier = Modifier.width(400.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                Button( // TODO : Change to Image Button
+                    modifier = Modifier
+                        .width(28.dp)
+                        .height(28.dp),
+                    onClick = onDialogDismiss
+                ) {
+                    Text(text = "X")
+                }
+                Spacer(modifier = Modifier.height(40.dp))
+                Box {
+                    Box(
+                        modifier = Modifier
+                            .width(400.dp)
+                            .height(424.dp)
+                            .align(Alignment.Center)
+                            .background(Color.LightGray)
+                            .border(2.dp, Color.Gray)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(352.dp)
+                                    .height(64.dp)
+                                    .align(Alignment.TopCenter)
+                                    .shadow(10.dp)
+                                    .background(
+                                        Color.White,
+                                        RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp)
+                                    ),
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .align(Alignment.TopStart)
+                                    .offset(24.dp, 8.dp),
+                                text = "총",
+                                fontSize = 28.sp
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .align(Alignment.TopEnd)
+                                    .offset((-24).dp, 8.dp),
+                                text = "${product.price}원",
+                                fontSize = 28.sp
+                            )
+                        }
+                        Button(
+                            modifier = Modifier
+                                .width(400.dp)
+                                .height(64.dp),
+                            shape = RectangleShape,
+                            onClick = { /*TODO*/ }
+                        ) {
+                            Text(
+                                text = "장바구니 추가",
+                                fontSize = 32.sp
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -216,5 +402,22 @@ fun ProductScreenPreview() {
             productListStateFlow = MutableStateFlow<List<ProductModel>>(emptyList()).asStateFlow(),
             optionListStateFlow = MutableStateFlow<List<OptionModel>>(emptyList()).asStateFlow()
         )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.TABLET)
+@Composable
+fun ProductDetailDialogPreview() {
+    OlymPosTheme{
+        ProductDetailDialogContent(
+            ProductModel(
+                0, 0, 0,
+                "메뉴명", 10000, "",
+                "메뉴 설명", 1, emptyList()
+            )
+        ) {}
     }
 }
